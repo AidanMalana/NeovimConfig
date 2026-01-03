a1472fe38a8233f5020f7a891b20be497fd307e4
Aidan Malana <aidanmalana02@gmail.com>
2026-01-03 17:41:01 -0600

    grayscale

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -31,13 +31,13 @@ require("lazy").setup({
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "gruvbox" } },
+  install = { colorscheme = { "zenwritten" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
 
 -- Set colorscheme
-vim.cmd("colorscheme gruvbox")
+vim.cmd("colorscheme zenwritten")
 
 local capabilities = {
   textDocument = {
